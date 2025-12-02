cc4bddbf025683c49749d7c64abd19dcbc75ec00
franpek <pedreirafran@gmail.com>
2025-12-02 23:33:28 +0100

    feat: day 2 part 2

--- apps/d02_gift_shop/lib/d02_gift_shop.ex
+++ apps/d02_gift_shop/lib/d02_gift_shop.ex
@@ -4,7 +4,8 @@ defmodule GiftShop do
   """
 
   @doc """
-  Method to sum the invalid ids located sequence of ranges, knowing that the invalid ones are the palindromic
+  Method to sum the invalid ids located sequence of ranges, knowing that the invalid ones have an even number of digits
+  and their right and left side are equal
 
   ## Examples
 
@@ -12,30 +13,29 @@ defmodule GiftShop do
       1227775554
 
       iex> GiftShop.sum_invalid_ids("files/sample.txt")
-      xxx
+      38310256125
 
   """
   def sum_invalid_ids(path) do
-    range_ids =
+    range_ids_sequences =
       File.read!(path)
       |> String.split(",", trim: true)
       |> Enum.map(fn range -> String.split(range, "-", trim: true) |> List.to_tuple() end)
       |> Enum.map(fn {first_id, second_id} ->
         String.to_integer(first_id)..String.to_integer(second_id)
       end)
-      |> IO.inspect()
 
-    range_ids
+    range_ids_sequences
     |> Enum.map(&filter_invalid_ids/1)
     |> List.flatten()
     |> Enum.sum()
   end
 
-  def filter_invalid_ids(range), do: Enum.filter(range, fn id -> not is_valid(id) end)
+  defp filter_invalid_ids(range), do: Enum.filter(range, fn id -> invalid?(id) end)
 
-  def is_valid(num), do: !is_palindromic(num)
+  defp invalid?(num), do: repeated_halves?(num)
 
-  def is_palindromic(num) do
+  defp repeated_halves?(num) do
     num_string = Integer.to_string(num)
     num_length = String.length(num_string)
 
@@ -43,5 +43,69 @@ defmodule GiftShop do
       String.split_at(num_string, div(num_length, 2))
 
     first_half == second_half
+  end
+
+  @doc """
+  Method to sum the really invalid ids located sequence of ranges, knowing that the invalid ones are a sequence of
+  digits repeated at least twice
+
+  ## Examples
+
+      iex> GiftShop.sum_really_invalid_ids("files/example.txt")
+      4174379265
+
+      iex> GiftShop.sum_really_invalid_ids("files/sample.txt")
+      58961152806
+
+  """
+  def sum_really_invalid_ids(path) do
+    range_ids_sequences =
+      File.read!(path)
+      |> String.split(",", trim: true)
+      |> Enum.map(fn range -> String.split(range, "-", trim: true) |> List.to_tuple() end)
+      |> Enum.map(fn {first_id, second_id} ->
+        String.to_integer(first_id)..String.to_integer(second_id)
+      end)
+
+    range_ids_sequences
+    |> Enum.map(&filter_really_invalid_ids/1)
+    |> List.flatten()
+    |> Enum.sum()
+  end
+
+  defp filter_really_invalid_ids(range), do: Enum.filter(range, fn id -> really_invalid?(id) end)
+
+  defp really_invalid?(id), do: deep_repeated_halves?(id)
+
+  defp deep_repeated_halves?(id) do
+    id_string = Integer.to_string(id)
+
+    # A list with the possible parts the id can be divided on to check repeated sequences of digits
+    id_divisible_parts = id_string |> id_divisible_parts
+
+    id_divisible_parts
+    |> Enum.any?(&are_all_parts_equal_in_sequences_of?(id_string, &1))
+  end
+
+  defp id_divisible_parts(id_string) do
+    id_length = String.length(id_string)
+
+    1..id_length
+    |> Enum.filter(fn part_len ->
+      rem(id_length, part_len) == 0 and div(id_length, part_len) >= 2
+    end)
+  end
+
+  defp are_all_parts_equal_in_sequences_of?(id_string, sequence) do
+    repeats = div(String.length(id_string), sequence)
+
+    id_string
+    |> String.graphemes()
+    |> Enum.chunk_every(sequence)
+    |> Enum.take(repeats)
+    |> case do
+      [] -> false
+      [first | rest] -> Enum.all?(rest, &(&1 == first))
+    end
   end
 end
