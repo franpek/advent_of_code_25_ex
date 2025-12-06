d593a639390a4b43839aabfb05dee7df0fa2cfc1
franpek <pedreirafran@gmail.com>
2025-12-06 18:00:29 +0100

    feat: day 6 part 2

--- apps/d06_trash_compactor/lib/d06_trash_compactor.ex
+++ apps/d06_trash_compactor/lib/d06_trash_compactor.ex
@@ -12,7 +12,7 @@ defmodule TrashCompactor do
       4277556
 
       iex> TrashCompactor.solve_math_worksheet("files/sample.txt")
-      xxx
+      6417439773370
 
   """
   def solve_math_worksheet(path) do
@@ -42,4 +42,70 @@ defmodule TrashCompactor do
 
   def operator_fun("+"), do: &Kernel.+/2
   def operator_fun("*"), do: &Kernel.*/2
+
+  @doc """
+  Method to solve the math worksheet of the cephalopod, knowing that math is written right-to-left in columns
+
+  ## Examples
+
+      iex> TrashCompactor.solve_math_worksheet_correctly("files/example.txt")
+      3263827
+
+      iex> TrashCompactor.solve_math_worksheet_correctly("files/sample.txt")
+      11044319475191
+
+  """
+  def solve_math_worksheet_correctly(path) do
+    worksheet_columns =
+      File.read!(path)
+      |> String.split("\r\n", trim: true)
+      |> Enum.map(&String.codepoints/1)
+      |> pad_to_max_length()
+      |> Enum.zip()
+      |> split_on_blank_row
+
+    worksheet_columns
+    |> Enum.map(&solve_column_correctly/1)
+    |> Enum.sum()
+  end
+
+  defp pad_to_max_length(lists) do
+    max_len = lists |> Enum.map(&length/1) |> Enum.max()
+    Enum.map(lists, fn l -> l ++ List.duplicate(" ", max_len - length(l)) end)
+  end
+
+  def split_on_blank_row(list) do
+    list
+    |> Enum.reduce({[], []}, fn tuple, {acc, current} ->
+      if Tuple.to_list(tuple) |> Enum.all?(&(&1 == " ")) do
+        {acc ++ [current], []}
+      else
+        {acc, current ++ [tuple]}
+      end
+    end)
+    |> then(fn {acc, current} ->
+      if current != [], do: acc ++ [current], else: acc
+    end)
+  end
+
+  def solve_column_correctly(col) do
+    operator =
+      col
+      |> hd
+      |> Tuple.to_list()
+      |> List.last()
+
+    numbers =
+      col
+      |> Enum.map(fn tuple ->
+        tuple
+        |> Tuple.to_list()
+        # remove spaces and operator
+        |> Enum.reject(&(&1 in [" ", "+", "*"]))
+        |> Enum.join()
+      end)
+      |> Enum.map(&String.to_integer/1)
+
+    Enum.reduce(numbers, operator_fun(operator))
+  end
 end
